d93dae3809665ecffca2d8d148d46cbe7d381841
yash nirmal <yash15nirmal@gmail.com>
2023-04-30 18:47:46 +0530

    final

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,45 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { useState } from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
-const inter = Inter({ subsets: ['latin'] })
+const data =[
+  {
+    id:"0",
+    name:"Luffy"
+  },
+  {
+    id:"1",
+    name:"Zoro"
+  },
+  {
+    id:"2",
+    name:"Sanji"
+  },
+  {
+    id:"3",
+    name:"Ussop"
+  },
+  {
+    id:"4",
+    name:"Nami"
+  }
+]
 
 export default function Home() {
+
+  const [characters,setCharacters]= useState(data)
+
+  function handleDrag(result){
+    console.log(result)
+
+    const temp = Array.from(characters)
+    const [reorderedItem] = temp.splice(result.source.index,1)
+    temp.splice(result.destination.index,0,reorderedItem)
+
+    setCharacters(temp)
+  }
+
+
   return (
     <>
       <Head>
@@ -14,100 +48,33 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="list-cont">
+        <DragDropContext onDragEnd={handleDrag}>
+          <Droppable droppableId="characters">
+            {
+              (provided)=>(
+                <div className="list" {...provided.droppableProps} ref={provided.innerRef}>
+                  {
+                    characters.map((el,idx)=>(
+                      <Draggable key={el.id} draggableId={el.id} index={idx} >
+                        {
+                          (provided)=>(
+                            <div className="list-item" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} >
+                              <p>id : {el.id}</p>
+                              <h3>{el.name}</h3>
+                            </div>
+                          )
+                        }
+                      </Draggable>
+                    ))
+                  }
+                  {provided.placeholder}
+                </div>
+              )
+            }
+            
+          </Droppable>
+        </DragDropContext>
       </main>
     </>
   )
